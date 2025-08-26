3f6d8e6c76bdd1833ec70944cc58ac19d3b2c44e
Peter Moelzer <moelzerpeter@gmail.com>
2025-08-26 19:34:42 +0200

    error detector

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNodet;
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
